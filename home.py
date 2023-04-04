1d50ee340094b920b81da2b60c35044f9579ffa2
deterius <bourim@gmail.com>
2023-04-04 22:50:48 +0800

    modified theme

--- home.py
+++ home.py
@@ -1,4 +1,17 @@
 import streamlit as st
+
+st.set_page_config (
+    page_title = 'Revenue Dashboard Example',
+    page_icon="🖖",
+    layout='wide',
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+
 import pandas as pd
 import numpy as np
 
@@ -8,14 +21,13 @@ import plotly.express as px
 
 
 
-
 st.title('Revenue Dashboard')
+st.subheader('Revenue Results per store')
 
 # date selectior
 date_options = st.selectbox(
     'Select date view:',
     ['week_num','month','date'])
-st.write(date_options)
 
 dfx = pd.read_excel('data/xtd.xlsx',engine='openpyxl', header=12)
 dfk = pd.read_excel('data/kc.xlsx',engine='openpyxl', header=12)
@@ -55,9 +67,39 @@ drop_cols = ['date','week_num','month','year','Checks', 'Discounts', 'Gross Sale
 if date_options != 'date':
     drop_cols.append('week_day')
 
+# Metric results
+df_metric = df.groupby(date_options).sum()
+
+rev_current = df_metric['Net Sales'].iloc[-1]
+rev_prev = df_metric['Net Sales'].iloc[-2]
+rev_delta = rev_current - rev_prev
+
+guest_current = df_metric['Guests'].iloc[-1]
+guest_prev = df_metric['Guests'].iloc[-2]
+guest_delta = float(guest_current - guest_prev)
+
+col1, col2, = st.columns(2)
+with col1:
+    st.metric("Net Revenue", rev_current, delta=rev_delta, delta_color='normal', help='Change in revenue from previous time period')
+with col2:
+    st.metric("Guest Count", guest_current, delta=guest_delta, delta_color='normal', help='Change in guests from previous time period')
 
 dftotal = hp.main_sum_org(df, date_options, drop_cols, None)
 
-st.subheader('Total (SUM)')
-st.write(dftotal)
+
+
+
+
+# main df
+with col1: 
+    st.markdown(' *** ')
+    st.subheader('Total (SUM)')
+    st.dataframe(dftotal)
+with col2:
+    # revenue chart
+    st.markdown(' **** ')
+    st.line_chart(df_metric['Net Sales'].reset_index(), x=date_options)
+    # guest count chart
+    st.bar_chart(df_metric['Guests'].reset_index(), x=date_options)
+
 
